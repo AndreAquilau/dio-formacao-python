ac5f42b141578f09069dfc2e4b9d8ebfb1ac1a35
andreaquilau <andre.aquilau@outlook.com>
2023-11-09 02:13:39 -0400

    init

---
+++ TypesPython/main.py
@@ -0,0 +1,3 @@
+from tipo_dados import name
+
+print("Teste: " + name)
---
+++ TypesPython/tipo_dados.py
@@ -0,0 +1,43 @@
+name : str = "André Da Silva Aquilau"
+age : int = 23
+weight : float = 66.00
+money : complex = 450_000.00
+listNumberInt : list = [ 10, 20, 30, 40, 50]
+array : range = [ 10, 20, 30 ]
+tupla : tuple = [ 30, 40, 50 ]
+mapa : dict = {
+    "name" : "André Da Silva"
+}
+
+mapa.update({"name": "aquilau"})
+
+print(listNumberInt[0])
+print(array[0])
+print(tupla[0])
+
+print(name[0][0])
+print(age)
+print(weight)
+print(money)
+print(mapa.get("name"))
+
+
+testTrue : bool = True
+testFalse : bool = False
+
+print(testTrue)
+print(testFalse)
+
+"""
+
+Comentário
+
+"""
+'''
+
+Comentário
+
+'''
+
+print("teste")
+print('python')
